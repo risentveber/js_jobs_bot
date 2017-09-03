2c3bb444612cc52384a06ac923f180ad1eca4431
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-09-03 22:54:07 +0300

    refactor

--- bot.js
+++ bot.js
@@ -2,38 +2,21 @@ const Slimbot = require('slimbot');
 const config = require('./config.json');
 const bot = new Slimbot(config.TELEGRAM_API_KEY);
 
-bot.on('message', message => {
-    console.log('message', message);
-});
+[
+    'message',
+    'edited_message',
+    'channel_post',
+    'edited_channel_post',
+    'callback_query',
+    'inline_query',
+    'chosen_inline_result',
+].forEach(type => bot.on(msgType, msg => console.log(msgType, msg)));
 
-bot.on('edited_message', message => {
-    console.log('edited_message', message);
-});
-
-bot.on('channel_post', post => {
-    console.log('channel_post', post);
-});
-
-bot.on('edited_channel_post', post => {
-    console.log('edited_channel_post', post);
-});
-
-bot.on('callback_query', query => {
-    console.log('edited_channel_post', query);
-});
-
-bot.on('inline_query', query => {
-    console.log('inline_query', query);
-});
-
-bot.on('chosen_inline_result', result => {
-    console.log('chosen_inline_result', result);
-});
 
 bot.startPolling();
 
 function logErrorToAdmin(err) {
-    bot.sendMessage(config.ADMIN_USER, `<b>Erorr</b>\n<code>${err}</code>`, { parse_mode: 'HTML' });
+    bot.sendMessage(config.ADMIN_USER, `<b>Error</b>\n<code>${err}</code>`, { parse_mode: 'HTML' });
 }
 
 function logMessageToAdmin(message) {
@@ -43,7 +26,7 @@ function logMessageToAdmin(message) {
 function postVacancy(message) {
     bot.sendMessage(config.TARGET_CHANNEL, message,
         { parse_mode: 'HTML', disable_web_page_preview: true }
-        );
+    );
 }
 
 module.exports = {
