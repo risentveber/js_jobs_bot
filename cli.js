8ef07f5ee7caff4bf45a53d1cae3d62bced8225b
Boris Strelnikov <risentveber@gmail.com>
2020-01-18 18:57:00 +0300

    fix cli

--- cli.js
+++ cli.js
@@ -2,13 +2,15 @@ const config = require('./config.json'); // eslint-disable-line
 const HhAdapter = require('./src/adapters/hh');
 const MoikrugAdapter = require('./src/adapters/moikrug');
 const { TelegramAPI } = require('./src/bot');
+const { LoadJobPageDOM } = require('./src/jobLoader');
 
 const api = new TelegramAPI(config);
 
 const url = process.argv[2];
 
 async function publishByLink(link, adapter) {
-    const vacancyText = await adapter.parseItem({ link });
+    const document = await LoadJobPageDOM(link);
+    const vacancyText = adapter.parseItem({ link, document });
     await api.postVacancy(vacancyText, link);
 }
 
